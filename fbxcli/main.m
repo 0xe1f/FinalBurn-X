73f3c053b96f615c8253dbdf4de1c5158f17f3e1
Akop Karapetyan <dev@akop.org>
2016-10-30 21:40:57 -0700

    Added fbxcli target

---
+++ fbxcli/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  fbxcli
+//
+//  Created by Akop Karapetyan on 10/30/16.
+//  Copyright © 2016 Akop Karapetyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
